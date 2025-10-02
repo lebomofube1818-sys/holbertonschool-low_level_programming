714579cbd224fe5929312ecb396c823fe42480a9
klehloenya <keketsolehloenya516@gmail.com>
2025-10-02 15:24:16 +0200

    Fix Betty style for 2-strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,24 +2,28 @@
 
 /**
  * _strchr - locates a character in a string
- * @s: pointer to the string to search
- * @c: character to locate
+ * @s: the string to search
+ * @c: the character to locate
  *
- * Return: pointer to first occurrence of c in s, or NULL if not found
+ * Return: pointer to the first occurrence of @c in @s,
+ * or 0 if the character is not found
  */
 char *_strchr(char *s, char c)
 {
-    int i;
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] == c)
-            return (s + i);
-    }
+	if (c == '\0')
+	{
+		return (s);
+	}
 
-    if (c == '\0')
-        return (s + i);
-
-    return (0);
+	return (0);
 }
 
