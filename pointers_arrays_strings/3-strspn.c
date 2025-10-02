0dd64b38cfa75d48b1053ed32d6aab4498c43c9d
klehloenya <keketsolehloenya516@gmail.com>
2025-10-02 15:20:55 +0200

    Fix Betty style for 3-strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,27 +2,33 @@
 
 /**
  * _strspn - gets the length of a prefix substring
- * @s: string to check
- * @accept: bytes to match
+ * @s: string to search
+ * @accept: string containing the bytes to match
  *
- * Return: number of bytes in initial segment of s
- *         consisting only of bytes from accept
+ * Return: number of bytes in the initial segment of s
+ * which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j, count;
+	unsigned int i, j, count;
 
-    count = 0;
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-                break;
-        }
-        if (accept[j] == '\0')
-            return (count);
-        count++;
-    }
-    return (count);
+	count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			return (count);
+		}
+		count++;
+	}
+
+	return (count);
 }
+
