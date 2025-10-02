3927b753d3c6996cdf1d76a41f569c3513dbf109
klehloenya <keketsolehloenya516@gmail.com>
2025-10-02 15:08:12 +0200

    Fix Betty style for 1-memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,18 +2,21 @@
 
 /**
  * _memcpy - copies memory area
- * @dest: pointer to the destination memory area
- * @src: pointer to the source memory area
+ * @dest: memory area to copy to
+ * @src: memory area to copy from
  * @n: number of bytes to copy
  *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-        dest[i] = src[i];
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
 
-    return (dest);
+	return (dest);
 }
+
