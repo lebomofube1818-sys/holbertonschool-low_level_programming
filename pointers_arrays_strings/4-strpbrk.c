53ab0c0fbb166e50c1278293b08bab374a1a1a8b
klehloenya <keketsolehloenya516@gmail.com>
2025-10-02 14:47:53 +0200

    4-strpbrk: implemented _strpbrk function and updated main.h

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search
+ * @accept: the set of bytes to look for
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL (0) if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (0);
+}
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,8 +8,10 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 /* String functions */
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 /* Other functions */
 int _putchar(char c);
 
 #endif /* MAIN_H */
+
