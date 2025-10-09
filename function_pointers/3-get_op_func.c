f45f209c67ee65906ee74d971c7f7ed886f2bb9f
klehloenya <keketsolehloenya516@gmail.com>
2025-10-09 11:38:11 +0200

    Add 3-calc project: calculator with function pointers, Betty compliant

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * get_op_func - selects the correct function for an operator
+ * @s: operator string
+ *
+ * Return: pointer to function or NULL if invalid
+ */
+int (*get_op_func(char *s))(int, int)
+{
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+    if (!s)
+        return (NULL);
+
+    i = 0;
+    while (ops[i].op)
+    {
+        if (strcmp(ops[i].op, s) == 0)
+            return (ops[i].f);
+        i++;
+    }
+
+    return (NULL);
+}
+
