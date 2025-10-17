201bdeecf4dcd3ae43f6af0d367702caf5f8e6bd
klehloenya <keketsolehloenya516@gmail.com>
2025-10-17 14:22:17 +0200

    Fix palindrome: fully recursive, no loops, Betty compliant

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,36 +1,43 @@
 #include "main.h"
 
 /**
- * check_palindrome - helper to recursively check palindrome
- * @s: string
- * @start: start index
- * @end: end index
- * Return: 1 if palindrome, 0 if not
- */
-int check_palindrome(char *s, int start, int end)
+	* _strlen_recursion - returns the length of a string
+	* @s: string to measure
+	* Return: length of string
+	*/
+int _strlen_recursion(char *s)
 {
-    if (start >= end)
-        return (1);
-    if (s[start] != s[end])
-        return (0);
-    return (check_palindrome(s, start + 1, end - 1));
+	if (*s == '\0')
+	return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * is_palindrome - returns 1 if string is a palindrome
- * @s: string to check
- * Return: 1 if palindrome, 0 if not
- */
+	* check_palindrome - helper to check palindrome recursively
+	* @s: string to check
+	* @start: start index
+	* @end: end index
+	* Return: 1 if palindrome, 0 if not
+	*/
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+	return (1);
+	if (s[start] != s[end])
+	return (0);
+	return (check_palindrome(s, start + 1, end - 1));
+}
+
+/**
+	* is_palindrome - returns 1 if string is a palindrome, 0 if not
+	* @s: string to check
+	* Return: 1 if palindrome, 0 if not
+	*/
 int is_palindrome(char *s)
 {
-    int len = 0;
+	if (*s == '\0')
+	return (1);
 
-    if (!s)
-        return (0);
-
-    while (s[len] != '\0')
-        len++;
-
-    return (check_palindrome(s, 0, len - 1));
+	return (check_palindrome(s, 0, _strlen_recursion(s) - 1));
 }
 
