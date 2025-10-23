bbbd2b09eeaaa0e8101c3ac32817f9c0a1a6025c
klehloenya <keketsolehloenya516@gmail.com>
2025-10-23 11:39:50 +0200

    Task 3: add_node_end (or brief message)

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,17 +3,18 @@
 
 /**
  * print_name - prints a name using a function pointer
- * @name: pointer to the name string
- * @f: function pointer that takes a char * argument
+ * @name: Pointer to the name string
+ * @f: Pointer to a function that takes a char * argument
  *
  * Description: This function checks if @name and @f are not NULL,
  * then calls the function pointed to by @f with @name as argument.
+ * If @name or @f is NULL, the function does nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
+    if (name == NULL || f == NULL)
+        return;
 
-	f(name);
+    f(name);
 }
 
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+# Holberton Low-Level Programming
+
+## Functions, Nested Loops
+
+This project contains exercises for the "functions and nested loops" section.  
+The first task prints "_putchar" followed by a new line using the `_putchar` function.
