837731758794ffd8b2c95ab5d2fea902633789d7
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2025-11-11 15:57:55 +0200

    Fix: corrected indentation and Betty style for 0-reset_to_98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of an integer to 98
- * @n: pointer to the integer to update
- *
- * Return: void
- */
+	* reset_to_98 - updates the value of an integer to 98
+	* @n: pointer to the integer to update
+	*
+	* Return: void
+	*/
 void reset_to_98(int *n)
 {
-    *n = 98;
+*n = 98;
 }
 
