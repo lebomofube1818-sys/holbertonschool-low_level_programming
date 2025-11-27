6900c2c706293c26002200a5fdd4788760a6161a
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2025-11-27 14:00:55 +0200

    6. Implement puts2 - print every other char (Betty style)

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
+
