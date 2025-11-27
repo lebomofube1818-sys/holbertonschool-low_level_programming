93b6b4e525968dff3d0bee96ffef3cc5c4fcaca0
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2025-11-27 14:11:02 +0200

    Task 7: Implement puts_half function

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int start;
+	int i;
+
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2 == 0)
+		start = length / 2;
+	else
+		start = (length + 1) / 2;
+
+	for (i = start; i < length; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
