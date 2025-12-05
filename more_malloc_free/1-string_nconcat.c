8fefd4a0767d6b66b9e0b088b359ab30309b40bd
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2025-12-05 12:32:17 +0200

    Fix Betty style: use tabs in 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,44 +2,43 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates s1 with the first n bytes of s2
+ * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: number of bytes to take from s2
+ * @n: number of bytes to concatenate from s2
  *
- * Return: pointer to newly allocated string, or NULL on failure
+ * Return: pointer to newly allocated space in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int len1 = 0, len2 = 0, i, j;
-    char *concat;
+	char *ptr;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-    while (s2[len2] != '\0')
-        len2++;
+	if (n >= len2)
+		n = len2;
 
-    if (n >= len2)
-        n = len2;
+	ptr = malloc(sizeof(char) * (len1 + n + 1));
+	if (ptr == NULL)
+		return (NULL);
 
-    concat = malloc(sizeof(char) * (len1 + n + 1));
-    if (concat == NULL)
-        return (NULL);
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
 
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
+	for (j = 0; j < n; j++)
+		ptr[i + j] = s2[j];
 
-    for (j = 0; j < n; j++, i++)
-        concat[i] = s2[j];
+	ptr[i + j] = '\0';
 
-    concat[i] = '\0';
-
-    return (concat);
+	return (ptr);
 }
 
